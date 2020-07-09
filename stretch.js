b98aee127fd2f058b5dc2c2589dae185623d0d97
mrjoshuamartinez <mrjoshuamartinez@gmail.com>
2020-07-09 17:04:12 -0400

    did some of the stretches

--- stretch.js
+++ stretch.js
@@ -11,18 +11,21 @@ See if you can complete one or more of the following challenges:
 (function(){
   var a = b = 3;
 })();
-console.log("a defined? " + (typeof a !== 'undefined'));
-console.log("b defined? " + (typeof b !== 'undefined'));
+console.log("a defined? " + (typeof a !== 'undefined')); // False
+console.log("b defined? " + (typeof b !== 'undefined')); // True
 
 /*
 
 2. Write a function that would allow you to do this using a closure. (This is another interview question we've seen before - when you're ready for answers, view an explanation [here](https://www.coderbyte.com/algorithm/3-common-javascript-closure-questions)).
+*/
+
 
 
 var addSix = createBase(6);
 addSix(10); // returns 16
 addSix(21); // returns 27
 
+/*
 3. Research the differences between functional programming and object oriented programming. Then, describe the pros and cons of functional programming vs object-oriented programming. This is a common interview question and great practice!
 
 ## Resources
